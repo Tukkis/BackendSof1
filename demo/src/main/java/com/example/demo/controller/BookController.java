898f01f480e4428160f0523955797a94369cbaa6
Tukkis <54272662+Tukkis@users.noreply.github.com>
2023-09-20 23:02:01 +0300

    add add book and delete book endpoints and functionality

--- demo/src/main/java/com/example/demo/controller/BookController.java
+++ demo/src/main/java/com/example/demo/controller/BookController.java
@@ -3,9 +3,12 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.domain.Book;
 import com.example.demo.domain.BookRepository;
 
 @Controller
@@ -23,6 +26,24 @@ public class BookController {
 	public String list(Model model) {
 		model.addAttribute("books",  repository.findAll());
 		return "booklist";
+	}
+	
+	@RequestMapping(value = "/add")
+	public String addBook(Model model){
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public String saveBook(Book book){
+		repository.save(book);
+		return "redirect:booklist";
+	}
+
+	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+		repository.deleteById(bookId);
+		return "redirect:../booklist";
 	}
 	
 }
