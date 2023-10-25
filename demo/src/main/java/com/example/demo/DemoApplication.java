25a97c0d9f0a8cd010c490206b7eba692804208e
Tukkis <54272662+Tukkis@users.noreply.github.com>
2023-10-25 15:41:09 +0300

    add dockerfile

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,9 @@ import com.example.demo.domain.Book;
 import com.example.demo.domain.BookRepository;
 import com.example.demo.domain.Category;
 import com.example.demo.domain.CategoryRepository;
+import com.example.demo.domain.User;
+import com.example.demo.domain.UserRepository;
+
 
 @SpringBootApplication
 public class DemoApplication {
@@ -18,7 +21,7 @@ public class DemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner categorycl(CategoryRepository crepository, BookRepository brepository) {
+	public CommandLineRunner categorycl(CategoryRepository crepository, BookRepository brepository, UserRepository urepository) {
 		return (args) -> {
 			Category a1 = new Category("scifi");
 			Category b1 = new Category("comic");
@@ -29,6 +32,11 @@ public class DemoApplication {
 			Book b2 = new Book("Petteri Perä", "Hello World", 1999, "894098-5432", 9, crepository.findByName("comic").get(0));
 			
 			brepository.save(b2);
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "a@b.c", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "a@b.c", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 		};
 	}
 }
--- demo/src/main/java/com/example/demo/WebSecurityConfig.java
+++ demo/src/main/java/com/example/demo/WebSecurityConfig.java
@@ -1,29 +1,32 @@
 package com.example.demo;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.example.demo.controller.UserDetailServiceImpl;
+
 @Configuration
-@EnableMethodSecurity
+@EnableMethodSecurity(securedEnabled =  true)
 public class WebSecurityConfig {
+	@Autowired
+    private UserDetailServiceImpl userDetailsService;
+	
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 		http
 		.authorizeHttpRequests( authorize -> authorize	
+			.requestMatchers(antMatcher("/css/**")).permitAll()
 			.anyRequest().authenticated()
 		)
 		.formLogin( formlogin -> formlogin
-			.loginPage("/login")
 			.defaultSuccessUrl("/booklist", true)
 			.permitAll()
 		)
@@ -33,24 +36,9 @@ public class WebSecurityConfig {
 		return http.build();
 	}
 	
-	@Bean
-	public UserDetailsService userDetailsService() {
-		List<UserDetails> users =  new ArrayList<UserDetails>();
-		UserDetails user = User.withDefaultPasswordEncoder()
-			.username("user")
-			.password("password")
-			.roles("USER")
-			.build();
-		users.add(user);
-		
-		user = User.withDefaultPasswordEncoder()
-				.username("admin")
-				.password("password")
-				.roles("ADMIN")
-				.build();
-		users.add(user);
-		
-		return new InMemoryUserDetailsManager(users);
-	}
+	@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+    }
 
 }
